53250238c35ee637179caf58a1edb7762574ae0a
likvidov <likvidya@gmail.com>
2020-02-07 13:19:32 +0300

    Адаптивная верстка

--- js/script.js
+++ js/script.js
@@ -1,8 +1,21 @@
 var mySwiper = new Swiper('.swiper-container', {
     speed: 400,
-    slidesPerView: 2,
+    slidesPerView: 1,
     loop: true,
     navigation: {
         nextEl: '.arrow'
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 2
+        }
     }
+});
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.header');
+
+menuButton.addEventListener('click', (event) => {
+    menuButton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
 });
